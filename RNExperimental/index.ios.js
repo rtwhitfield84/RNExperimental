bf783f680e0d56891ecfc9524181ca93c3d4bbcd
Rico <richardthomaswhitfield@gmail.com>
2017-04-05 14:46:32 -0500

    nav constructor

--- RNExperimental/index.ios.js
+++ RNExperimental/index.ios.js
@@ -35,9 +35,56 @@ const NavReducer = createReducer({
 })
 
 class RNExperimental extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      navState: NavReducer(undefined, {})
+    }
+  }
+  _handleAction (action) {
+    const newState = NavReducer(this.state.navState, action);
+    if (newState === this.state.navState) {
+      return false;
+    }
+    this.setState({
+      navState: newState
+    })
+    return true;
+ }
+ handleBackAction() {
+   return this._handleAction({ type: 'pop' });
+ }
+ _renderRoute (key) {
+  if (key === 'Home') {
+    return <Home
+             onPress={this._handleAction.bind(this,
+             { type: 'push', key: 'About' })} /> 
+  }
+  if (key === 'About') {
+    return <About
+            goBack={this.handleBackAction.bind(this)}
+            onPress={this._handleAction.bind(this,
+            { type: 'push', key: 'Contact' })} />
+  }
+  if (key === 'Contact') {
+    return <Contact
+             goBack={ this.handleBackAction.bind(this)} />
+  }
+}
+_renderScene(props) {
+    const ComponentToRender = this._renderRoute(props.scene.route.key)
+    return (
+      <ScrollView style={styles.scrollView}>
+        {ComponentToRender}
+      </ScrollView>
+    );
+}
   render() {
     return (
-      <Home  />
+      <NavigationCardStack
+        navigationState={this.state.navState}
+        onNavigate={this._handleAction.bind(this)}
+        renderScene={this._renderScene.bind(this)} />
     )
   }
 }
@@ -70,17 +117,18 @@ const Contact = ({ goBack }) => (
 )
 const styles = StyleSheet.create({
   scrollView: {
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#f1c40f',
     flex: 1
   },
   container: {
     flex: 1,
-    backgroundColor: '#F5FCFF'
+    backgroundColor: '#f1c40f'
   },
   title: {
     fontSize: 40,
     marginTop: 200,
-    textAlign: 'center'
+    textAlign: 'center',
+    color: '#2c3e50'
   },
   button: {
     height: 70,
@@ -89,7 +137,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginLeft: 20,
     marginRight: 20,
-    backgroundColor: '#EDEDED'
+    borderRadius: 20,
+    backgroundColor: '#d35400'
   }
 })
 AppRegistry.registerComponent('RNExperimental', () => RNExperimental)
